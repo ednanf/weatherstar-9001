fadaadb4d002af4713a4ab9630d79a8d90a51539
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-09-26 12:45:04 -0300

    Add header and logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,8 @@ import './Header.css';
 const Header = () => {
   return (
     <div id='header-container'>
-      <div className='header-contents'>
-        <div id='header-bar'></div>
-        <img src={Logo} alt='' id='header-logo' />
-      </div>
+      <div id='header-bar'></div>
+      <img src={Logo} alt='' id='header-logo' />
     </div>
   );
 };
