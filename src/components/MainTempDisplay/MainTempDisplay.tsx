9b0ead0a9bc556c3f518c265147fe504470f4e23
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-11-07 12:39:08 -0300

    Make hook to capitalize letters correctly.

--- src/components/MainTempDisplay/MainTempDisplay.tsx
+++ src/components/MainTempDisplay/MainTempDisplay.tsx
@@ -1,4 +1,6 @@
 import { useIcon } from '../../hooks/useIcon';
+import useCapitalizeFirstLetter from '../../hooks/useCapitalizeFirstLetter';
+
 import './MainTempDisplay.css';
 
 interface MainTempDisplayProps {
@@ -15,7 +17,7 @@ const MainTempDisplay: React.FC<MainTempDisplayProps> = ({ temp, desc, iconCode 
           <p>{Math.ceil(temp)}</p>
           <span id='main-degrees'>º</span>
         </div>
-        <p id='main-conditions'>{desc}</p>
+        <p id='main-conditions'>{useCapitalizeFirstLetter(desc)}</p>
         <div id='main-weather-icon'>{useIcon(iconCode, 'lg')}</div>
       </div>
     </div>
