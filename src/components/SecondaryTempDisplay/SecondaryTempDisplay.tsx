80aa7e69cb8cab6be40e197d285824bb2bb0b58a
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-11-07 11:39:00 -0300

    Implement city name normalization

--- src/components/SecondaryTempDisplay/SecondaryTempDisplay.tsx
+++ src/components/SecondaryTempDisplay/SecondaryTempDisplay.tsx
@@ -11,13 +11,21 @@ interface SecondaryTempDisplayProps {
 }
 
 const SecondaryTempDisplay: React.FC<SecondaryTempDisplayProps> = ({ city, feelsLike, wind, humidity, pressure }) => {
+  function removeAccents(word: String) {
+    return word.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
   return (
-    <div>
-      <p>{city}</p>
-      <p>Feels like: {Math.ceil(feelsLike)}°C</p>
-      <p>Wind speed: {Math.ceil(wind)} m/s</p>
-      <p>Humidity: {Math.floor(humidity)}%</p>
-      <p>Pressure: {pressure} hPa</p>
+    <div id='secondary-temp-container'>
+      <div id='secondary-city'>
+        <p>{removeAccents(city)}</p>
+      </div>
+      <div id='secondary-data'>
+        <p>Feels like: {Math.ceil(feelsLike)}°C</p>
+        <p>Wind speed: {Math.ceil(wind)} m/s</p>
+        <p>Humidity: {Math.floor(humidity)}%</p>
+        <p>Pressure: {pressure} hPa</p>
+      </div>
     </div>
   );
 };
