0df67a8c1cd2ac93b655c28f2437201977f39afd
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-11-08 11:38:35 -0300

    update function

--- src/hooks/useCapitalizeFirstLetter.tsx
+++ src/hooks/useCapitalizeFirstLetter.tsx
@@ -1,7 +1,14 @@
-export default function capitalizeFirstLetter(str: String) {
-  return str
-    .toLowerCase()
-    .split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(' ');
+/* 
+    The parameter has to be optional to accept an undefined value. It will only
+    return if it isn't, so it will always work. Error handling will be applied
+    as needed.
+*/
+export default function capitalizeFirstLetter(str?: String) {
+  if (str) {
+    return str
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
 }
