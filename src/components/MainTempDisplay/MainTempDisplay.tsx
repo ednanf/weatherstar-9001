8de52684e5316a3c35da7d69c6a6caa48b5f887b
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-11-12 18:55:11 -0300

    Fix main condition description not working

--- src/components/MainTempDisplay/MainTempDisplay.tsx
+++ src/components/MainTempDisplay/MainTempDisplay.tsx
@@ -18,7 +18,7 @@ const MainTempDisplay: React.FC<MainTempDisplayProps> = ({ temp, desc, iconCode 
           <p>{Math.ceil(temp)}</p>
           <span id='main-degrees'>º</span>
         </div>
-        <p id='main-conditions'>{desc ? useCapitalizeFirstLetter(useConditionCode(desc)) : '--'}</p>
+        <p id='main-conditions'>{useCapitalizeFirstLetter(useConditionCode(desc))}</p>
         <div id='main-weather-icon'>{useIcon(iconCode, 'lg')}</div>
       </div>
     </div>
