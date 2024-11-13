d0d1cd91ee34335b29df79bc8eb656dc09e7c648
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-11-13 14:56:25 -0300

    Add clock

--- src/components/ContentContainer/ContentContainer.tsx
+++ src/components/ContentContainer/ContentContainer.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 
 import CurrentWeatherDisplay from '../CurrentWeatherDisplay/CurrentWeatherDisplay';
+import Clock from '../Clock/Clock';
 
 import './ContentContainer.css';
 
@@ -9,11 +10,17 @@ interface ContentContainerProps {
   cityName: string;
 }
 
+// TODO: tweak the clock's position
+
 const ContentContainer: React.FC<ContentContainerProps> = ({ weatherData, cityName }) => {
   const memoizedWeatherData = useMemo(() => weatherData, [weatherData]);
 
   return (
     <div id='content-container'>
+      {/* Margin 10vw because it's the same as the content container */}
+      <div className='' style={{ position: 'absolute', right: '10vw', top: '5vh' }}>
+        <Clock />
+      </div>
       <CurrentWeatherDisplay weatherData={memoizedWeatherData} cityName={cityName} />
     </div>
   );
