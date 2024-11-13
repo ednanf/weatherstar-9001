4cd561c5dc1a47d51dd81a535d7fedd8f4d917ee
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2024-11-13 16:48:55 -0300

    Correct typescript

--- src/hooks/useDegreesToDirection.tsx
+++ src/hooks/useDegreesToDirection.tsx
@@ -6,7 +6,7 @@ const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
  * @param {number} number - The angle in degrees (0-360).
  * @returns {string} The corresponding cardinal direction (N, NE, E, SE, S, SW, W, NW).
  */
-const useDegreesToDirection = (number: number) => {
+const useDegreesToDirection = (number: number): string => {
   const index = Math.round((number % 360) / 45);
   return directions[index];
 };
